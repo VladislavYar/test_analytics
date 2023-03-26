6b13cb5cf0ee862f246deb9d521a157b258d389d
Владислав Ярёменко <vlad.yaremenko.98@yandex.ru>
2023-03-26 22:47:13 +0300

    Revert.

--- code_test.py
+++ code_test.py
@@ -1,7 +1,11 @@
 from sys import platform
 
 
+<<<<<<< HEAD
 print('Не очень опасные изменения!')
+=======
+print('Опасные изменения.')
+>>>>>>> parent of 36987dd (Main.)
 if platform != 'win32':
     print('Любитель линукс или яблока?')
 if platform == 'win32':
