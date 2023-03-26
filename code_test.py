b2c016bb92a88cb0e8b6a4aabc19923891ad43ee
Владислав Ярёменко <vlad.yaremenko.98@yandex.ru>
2023-03-26 22:57:01 +0300

    Correct code.

--- code_test.py
+++ code_test.py
@@ -1,11 +1,7 @@
 from sys import platform
 
 
-<<<<<<< HEAD
-print('Не очень опасные изменения!')
-=======
 print('Опасные изменения.')
->>>>>>> parent of 36987dd (Main.)
 if platform != 'win32':
     print('Любитель линукс или яблока?')
 if platform == 'win32':
