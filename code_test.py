ee67f396ee79194bed0fafa8ce5082bf89c56b15
Владислав Ярёменко <vlad.yaremenko.98@yandex.ru>
2023-03-26 22:47:13 +0300

    git cherry-pick

--- code_test.py
+++ code_test.py
@@ -1,5 +1,13 @@
 from sys import platform
 
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+print('Не очень опасные изменения!')
+=======
+print('Опасные изменения.')
+>>>>>>> parent of 36987dd (Main.)
+>>>>>>> 6b13cb5 (Revert.)
 if platform != 'win32':
     print('Любитель линукс или яблока?')
